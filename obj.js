95ab84c764ed82a3ac384627f9966492728dca8f
elpozdyakov <54866113+elpozdyakov@users.noreply.github.com>
2020-04-16 17:52:32 +0300

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,26 @@
+let cityfirst = {
+    name: 'Vinnitsa',
+    population: 370834,
+    getName() {
+        console.log(`City ${this.name}`);
+    },
+    exportStr() {
+        console.log(`City ${this.name}, population = ${this.population}\n`);
+    },
+};
+let citysecond = {
+    name: 'Lugansk',
+    population: 403938,
+    getName() {
+        console.log(`City ${this.name}`);
+    },
+    exportStr() {
+        console.log(`City ${this.name}, population = ${this.population}\n`);
+    },
+};
+
+cityfirst.getName();
+citysecond.getName();
+
+cityfirst.exportStr();
+citysecond.exportStr();
